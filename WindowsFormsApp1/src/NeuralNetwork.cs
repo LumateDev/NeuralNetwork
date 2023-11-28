7b9ffa5491f72a2c3acbb09e6e728265a2c71fdc
Alex <Sanchous557@mail.ru>
2023-11-28 12:53:37 +0300

    init project

---
+++ WindowsFormsApp1/src/NeuralNetwork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace WindowsFormsApp1.src
+{
+    class NeuralNetwork
+    {
+        // Массив для хранения выходного сигнала нейросети
+        public double[] fact = new double[10];
+
+    }
+}
---
+++ WindowsFormsApp1/src/Neuron.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using static System.Math;
+
+namespace WindowsFormsApp1.src
+{
+    class Neuron
+    {
+
+        //поляч
+        private TypeNeuron typeNeuron;
+        private double[] inputs;
+        private double output;
+        private double[] weights;
+        private double derevative;
+
+        //свойства
+
+        public double[] Inputs { get => inputs; set => inputs = value; }
+        public double[] Weights { get => weights; set => weights = value; }
+        public double Output { get => output; }
+        public double Derevative { get => derevative; }
+
+
+        //constructor
+
+        public Neuron(double[] w, TypeNeuron t)
+        {
+            this.typeNeuron = t;
+            this.weights = w;
+        }
+
+        private double Lya_Kirily (double arg)
+        {
+            return Math.Max(0.01 * arg, arg);
+        }
+        private double Lya_Kirily_derevator(double arg)
+        {
+            if (arg > 0)
+            return 1;
+            else return 0.01;
+        }
+
+        public void Activator(double[] inpt, double[] wght)
+        {
+            double sum = wght[0];
+            for (int i = 0; i < inpt.Length; i++) { 
+                sum += inpt[i] * wght[i + 1];
+            }
+
+
+            switch(typeNeuron) {
+                case TypeNeuron.Hidden:output = Lya_Kirily(sum); derevative = Lya_Kirily_derevator(sum); break;
+                case TypeNeuron.Output:output = Exp(sum); break;
+            }  
+        }
+
+        
+
+    }
+}
